c2e290403113650dddf18c02ad007d4962db16e8
xxx <ddd>
2022-05-23 16:25:11 +0300

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,159 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="q-px-lg q-py-sm bg-white text-dark">
+      <div  class=" flex items-center justify-between">
+        <img  src="~assets/logo.png">
+        <q-tabs class="gt-sm"  indicator-color="transparent"
+                active-color="red-13"
+                v-model="curLink">
+
+          <q-route-tab  no-caps :ripple="false" v-for="(tab,index) in menuItems" :key="tab.title" :name="index" :label="tab.title" :to="tab.link" />
+
+        </q-tabs>
+        <div class="gt-sm q-gutter-sm">
+          <q-btn
+            flat
+            dense
+            round
+            icon="lab la-vk"
+          />
+          <q-btn
+            flat
+            dense
+            round
+            icon="lab la-telegram"
+          />
+          <q-btn
+            flat
+            dense
+            round
+            icon="lab la-youtube"
+          />
+
+        </div>
+        <q-btn
+          flat
+          dense
+          round
+          class="lt-md"
+          icon="menu"
+          aria-label="Menu"
+          @click="mobileMenuActive = !mobileMenuActive"
+        />
+
+      </div>
+
+
+
+    </q-header>
+
+    <q-drawer
+      v-model="mobileMenuActive"
+      behavior="mobile"
+      bordered
+      side="right"
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <footer class="bg-grey-9 q-py-lg text-white">
+      <div class="container">
+        <div class="row q-col-gutter-lg">
+          <div class="col-12 col-sm-6">
+            <img class="q-mb-lg" src="~assets/logo.png">
+            <div class="q-gutter-sm">
+              <q-btn
+                flat
+                dense
+                round
+                icon="lab la-vk"
+              />
+              <q-btn
+                flat
+                dense
+                round
+                icon="lab la-telegram"
+              />
+              <q-btn
+                flat
+                dense
+                round
+                icon="lab la-youtube"
+              />
+
+            </div>
+          </div>
+          <div class="col-12 col-sm-6">
+            <div class="footer-grid">
+              <p class="no-margin" v-for="tab in menuItems" :key="tab.title">
+                <router-link class="text-caption"  :to="tab.link">{{tab.title}}</router-link>
+              </p>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </footer>
+  </q-layout>
+</template>
+
+<script setup>
+
+import {ref} from "vue";
+
+const mobileMenuActive=ref(false)
+const curLink=ref('index')
+
+const menuItems = [
+  {
+    title: 'Главная',
+    link: '/'
+  },{
+    title: 'Преимущества',
+    link: '/'
+  },
+  {
+    title: 'FAQ',
+    link: '/'
+  },
+  {
+    title: 'Как это работает',
+    link: '/'
+  },
+  {
+    title: 'Переводы',
+    link: '/'
+  },
+  {
+    title: 'Обменник',
+    link: '/'
+  },
+  {
+    title: 'Решения для бизнеса',
+    link: '/'
+  },
+  {
+    title: 'Спец. предложение для НКО',
+    link: '/'
+  }
+]
+
+
+</script>
+<style lang="sass">
+.footer-grid
+  display: grid
+  grid-template-columns: 1fr
+  grid-gap: 5px
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page >
+    <section class="bg-grey-2 q-py-xl">
+      <div class="container">
+        <div class="row q-col-gutter-lg items-center">
+          <div class="col-12 col-sm-6">
+            <p class="text-h3 text-bold">Блокчейн<br class="gt-sm"> технологии</p>
+            <p>эволюция в транзакциях<br class="gt-sm">
+              с применением новых технологий<br class="gt-sm">
+              на собственном блокчейне</p>
+          </div>
+          <div class="col-12 col-sm-6">
+            <q-img src="~assets/offer_img.png"/>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="q-py-xl">
+      <div class="container">
+       <p class="text-h3 text-bold">Простое решение сложных задач</p>
+        <p>Мир не стоит на месте, по всюду блокировки свифт и популярных платежных ресурсов, мы создали решение для устранения ваших неудобств, при совершении или принятия платежей, при обменне цифровых собственностей, без посредников и ожидания</p>
+        <p>Запишитесь на тест-драйв, чтобы убедиться во всём лично.</p>
+      </div>
+    </section>
+
+  </q-page>
+</template>
+
+<script>
+
+</script>
