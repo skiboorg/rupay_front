fcc4fb7054cb63d4b64fe1a8c62e9e98840a707d
xxx <ddd>
2022-06-27 15:39:31 +0300

    rt

--- src/pages/marketplace/Chikenbro.vue
+++ src/pages/marketplace/Chikenbro.vue
@@ -3,7 +3,7 @@
     <section class="">
       <div class="container">
         <p class="text-h3 text-bold q-mb-lg">ЧикенБро</p>
-        <p class="text-h6 text-primary"><a style="text-decoration: underline" target="_blank" href="https://xn--k1affs.xn--p1ai/chicken_bro_info.pdf">Презентация ЧикенБро</a></p>
+        <p class="text-h6 text-primary"><a style="text-decoration: underline" target="_blank" href="/chicken_bro_info.pdf">Презентация ЧикенБро</a></p>
         <p class="text-h6">Токен чикен бро, выпущен ПКРГИ РОЛФ и обеспечен собственностью доли ПКРГИ РОЛФ в Компании Чикен Бро (50% доли). Реализация и выкуп данного актива возможны только с согласования совета правления кооператива РОЛФ, и средства, собранные с его реализации, направляются только на развитие компании Чикен Бро. Реализуя данный актив, кооператив РОЛФ обязуется произвести его обратный выкуп с выгодой для преобретателя, в соответствии с протоколом собрания кооператива.</p>
         <p class="text-h6"><strong>Выпущено актива:</strong> 10000000</p>
 
